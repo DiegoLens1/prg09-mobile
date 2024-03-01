f2e4d62696d06533c52994f5eb4718c5adbe32a2
Diego <d.lens2712@gmail.com>
2024-03-01 17:49:50 +0100

    added styling

--- components/recipeListRenderItem.js
+++ components/recipeListRenderItem.js
@@ -1,14 +1,25 @@
 import * as React from 'react'
-import { Text, Image } from "react-native"
+import { Text, Image, Pressable, StyleSheet } from "react-native"
 
 export default function RecipeListRenderItem({data}) {
     return(
-        <React.Fragment>
-            <Text>{data.strMeal}</Text>
+        <Pressable style={styles.listItemWrapper}>
             <Image
                 source={{uri: data.strMealThumb}}
                 style={{width: '30%', height: undefined, borderRadius: 10, aspectRatio: 1/1}}
             />
-        </React.Fragment>
+            <Text>{data.strMeal}</Text>
+        </Pressable>
     )
 }
+
+const styles = StyleSheet.create({
+    listItemWrapper: {
+        padding: 10,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: "#999",
+        flexDirection: "row",
+        gap: 10
+    }
+  });
+  
