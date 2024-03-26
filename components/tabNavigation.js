223ca099bde2ddb4ab13e08f2de287c86acb69c5
Diego <d.lens2712@gmail.com>
2024-03-26 10:30:22 +0100

    tab order changed

--- components/tabNavigation.js
+++ components/tabNavigation.js
@@ -8,9 +8,9 @@ const Tab = createMaterialBottomTabNavigator();
 export default function TabNavigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="All" component={AllRecipes} />
-      <Tab.Screen name="Saved" component={SavedRecipes} />
       <Tab.Screen name="Random" component={RandomRecipe} />
+      <Tab.Screen name="Local" component={AllRecipes} />
+      <Tab.Screen name="Saved" component={SavedRecipes} />
     </Tab.Navigator>
   );
 }
